afa7058d11cb150d6fd18489241eddd36c95a270
Chris Sprague <chris@chaoscontrol.org>
2014-04-25 21:23:57 -0400

    init package

---
+++ src/Badmushroom/Crumbler/CrumblerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Badmushroom\Crumbler;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrumblerServiceProvider extends ServiceProvider 
+{
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
